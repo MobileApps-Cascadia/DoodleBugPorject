8e9a28176d7679a9b2ebad6e536757a6fd4a8ea0
Fred Jaworski <fjaworski91@live.com>
2015-11-05 18:27:53 -0800

    Created drawing view class to handle user input for drawing. Set up basic touchevent handler. TODO: implement touchStart, touchMove and touchEnd.

--- app/src/main/java/edu/cascadia/doodlebug/DrawActivity.java
+++ app/src/main/java/edu/cascadia/doodlebug/DrawActivity.java
@@ -1,19 +1,15 @@
 package edu.cascadia.doodlebug;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
@@ -22,11 +18,14 @@ public class DrawActivity extends Activity {
 
     RelativeLayout drawLayout;
     int CAMERA_PIC_REQUEST = 2;
+    private DrawingView drawingView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_draw);
+
+        drawingView = (DrawingView) findViewById(R.id.drawingView);
 
         //prepare draw image that return from camera to layout background
         //need to convert picture to bitmap first.
---
+++ app/src/main/java/edu/cascadia/doodlebug/DrawingView.java
@@ -0,0 +1,84 @@
+package edu.cascadia.doodlebug;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class DrawingView extends View {
+
+    private Bitmap mBitmap; // bitmap for the canvas
+    private Canvas mCanvas; // canvas object to draw onto
+    private Path mPath; // path for drawing
+    private Paint mPaint; // paint to describe the line being drawn
+    private Context mContext;
+
+    public DrawingView(Context c) {
+        this(c, null);
+    }
+
+    public DrawingView(Context c, AttributeSet attr) {
+        super(c, attr);
+        mContext = c;
+        mPath = new Path();
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setColor(Color.BLACK);
+        mPaint.setStrokeWidth(2);
+        mPaint.setStyle(Paint.Style.STROKE);
+    }
+
+    @Override
+    public void onSizeChanged(int w, int h, int oldW, int oldH) {
+        super.onSizeChanged(w, h, oldW, oldH);
+        mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        mCanvas = new Canvas(mBitmap);
+    }
+
+    @Override
+    public void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        mCanvas.drawPath(mPath, mPaint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float x = event.getX();
+        float y = event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                touchStart(x, y);
+                invalidate();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                touchMove(x, y);
+                invalidate();
+                break;
+            case MotionEvent.ACTION_UP:
+                touchEnd();
+                invalidate();
+                break;
+        }
+
+        return false;
+    }
+
+    private void touchStart(float x, float y) {
+        // TODO: implement touchStart method
+    }
+
+    private void touchMove(float x, float y) {
+        // TODO: implement touchMove method
+    }
+
+    private void touchEnd() {
+        // TODO: implement touchEnd method
+    }
+}

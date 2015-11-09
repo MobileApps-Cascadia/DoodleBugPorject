91c467dd00da2e2246c3506debb90536e7522704
Justin Lange <well1912@gmail.com>
2015-11-09 13:28:37 -0800

    Added new ColorDialog fragment implementation

--- app/src/main/java/edu/cascadia/doodlebug/ColorDialog.java
+++ app/src/main/java/edu/cascadia/doodlebug/ColorDialog.java
@@ -7,27 +7,33 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
 
 public class ColorDialog extends Fragment {
-    private ColorListener mListener;
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String CURRENT_COLOR = "current color";
-    int mColor;
+    private int previousColor;
+    private ChangeColorListener mListener;
 
-    public static ColorDialog newInstance(int currentColor) {
+    public static ColorDialog newInstance(int color) {
         ColorDialog fragment = new ColorDialog();
         Bundle args = new Bundle();
-        args.putInt(CURRENT_COLOR, currentColor);
+        args.putInt(CURRENT_COLOR, color);
         fragment.setArguments(args);
         return fragment;
     }
 
-    public ColorDialog() { }
+    public ColorDialog() {
+        // Required empty public constructor
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mColor = getArguments().getInt(CURRENT_COLOR);
+            previousColor = getArguments().getInt(CURRENT_COLOR);
         }
     }
 
@@ -35,17 +41,44 @@ public class ColorDialog extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_color_dialog, container, false);
+        final View v = inflater.inflate(R.layout.fragment_color_dialog, container, false);
+        final RadioGroup r = (RadioGroup) v.findViewById(R.id.color_choices);
+
+        addColorChoice(r, "Black", Color.BLACK);
+        addColorChoice(r, "White", Color.WHITE);
+        addColorChoice(r, "Yellow", Color.YELLOW);
+        addColorChoice(r, "Blue", Color.BLUE);
+        addColorChoice(r, "Red", Color.RED);
+        addColorChoice(r, "Green", Color.GREEN);
+        addColorChoice(r, "Magenta", Color.MAGENTA);
+
+        return v;
+    }
+
+    void addColorChoice(RadioGroup group, String label, final int color) {
+        final RadioButton b = new RadioButton(getActivity());
+
+        b.setText(label);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mListener.onColorChange(color);
+            }
+        });
+        final boolean selected = color == previousColor;
+        if (!selected) b.setAlpha(0.5f);
+        b.setChecked(selected);
+        group.addView(b);
     }
 
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
         try {
-            mListener = (ColorListener) activity;
+            mListener = (ChangeColorListener) activity;
         } catch (ClassCastException e) {
             throw new ClassCastException(activity.toString()
-                    + " must implement ColorListener");
+                    + " must implement ChangeColorListener");
         }
     }
 
@@ -55,7 +88,7 @@ public class ColorDialog extends Fragment {
         mListener = null;
     }
 
-    public interface ColorListener {
-        public void onColorChange(int color);
+    public interface ChangeColorListener {
+        void onColorChange(int color);
     }
 }

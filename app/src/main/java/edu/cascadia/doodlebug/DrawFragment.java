91aaa5e83da55fcce36ed9bd796794b02609a3e6
Fred Jaworski <fjaworski91@live.com>
2015-12-03 19:45:25 -0800

    Removed Stickers and Camera Button

--- app/src/main/java/edu/cascadia/doodlebug/DrawFragment.java
+++ app/src/main/java/edu/cascadia/doodlebug/DrawFragment.java
@@ -39,12 +39,12 @@ public class DrawFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_draw, container, false);
-        v.findViewById(R.id.imageButttonTakePic).setOnClickListener(new View.OnClickListener() {
+        /*v.findViewById(R.id.imageButttonTakePic).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 takePhoto();
             }
-        });
+        }); */
 
         mView = (DrawingView) v.findViewById(R.id.drawingView);
         return v;
